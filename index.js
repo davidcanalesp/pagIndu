6d70ca76be36e07c2c09f88f48c0c4fb2930e997
David Canales Páez <david.canalespaez@gmail.com>
2021-06-25 02:02:56 -0500

    completadas las secciones About Us y Services

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ console.log('en el script');
 const btnHamburguesa = document.getElementById('btnHamburguesa');
 const sidebar = document.getElementById('sidebar');
 const contenedor = document.getElementById('contenedor');
-
+const encabezado = document.getElementById('encabezado');
 
 btnHamburguesa.addEventListener('click', ()=>{
     sidebar.classList.toggle ('active');
     contenedor.classList.toggle('active');
-    btnHamburguesa.classList.toggle('active')
-})
+    btnHamburguesa.classList.toggle('active');
+});
+
+window.addEventListener('scroll', ()=>{
+    encabezado.classList.toggle('sticky', window.scrollY>0 );
+});
