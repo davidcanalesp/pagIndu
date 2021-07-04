29ef7228ebce42b09dc8936c1797fdb385c5b58a
David Canales Páez <david.canalespaez@gmail.com>
2021-07-03 20:22:06 -0500

    completadas todas las secciones y vinculadas con el menú

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const btnHamburguesa = document.getElementById('btnHamburguesa');
 const sidebar = document.getElementById('sidebar');
 const contenedor = document.getElementById('contenedor');
 const encabezado = document.getElementById('encabezado');
+const vinculo = document.getElementById('vinculo');
+const linkHome= document.getElementById('linkHome');
+const linkNosotros= document.getElementById('linkNosotros');
+const linkServicios = document.getElementById('linkServicios');
+const linkClientes = document.getElementById ('linkClientes');
+const linkTestimonios = document.getElementById('linkTestimonios');
+const linkEquipo = document.getElementById('linkEquipo');
 
 btnHamburguesa.addEventListener('click', ()=>{
     sidebar.classList.toggle ('active');
@@ -12,4 +19,32 @@ btnHamburguesa.addEventListener('click', ()=>{
 
 window.addEventListener('scroll', ()=>{
     encabezado.classList.toggle('sticky', window.scrollY>0);
+});
+
+vinculo.addEventListener('click', ()=>{
+    btnHamburguesa.click();
+});
+
+linkHome.addEventListener('click', ()=>{
+    btnHamburguesa.click();
+});
+
+linkNosotros.addEventListener('click', ()=>{
+    btnHamburguesa.click();
+});
+
+linkServicios.addEventListener('click', ()=>{
+    btnHamburguesa.click();
+});
+
+linkClientes.addEventListener('click', ()=>{
+    btnHamburguesa.click();
+});
+
+linkTestimonios.addEventListener('click', ()=>{
+    btnHamburguesa.click();
+});
+
+linkEquipo.addEventListener('click', ()=>{
+    btnHamburguesa.click();
 });
